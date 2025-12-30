f72d3fc7152d406f9b3b9ec21fba2db21b2413c9
LangThong <thanhthuantanan@gmail.com>
2025-12-30 16:26:42 +0700

    UpdatecodeDay30/12

--- src/event-registration/main.js
+++ src/event-registration/main.js
@@ -3,6 +3,7 @@ const stepLocation = document.querySelector("#step-location")
 const nameInput = document.getElementById("name")
 const emailInput = document.getElementById("email")
 const phoneInput = document.getElementById("phone")
+const addressInput = document.getElementById("address")
 
 const confirmInfo= document.getElementById("confirm-info")
 
@@ -10,15 +11,21 @@ const errorName = document.getElementById("error-Name")
 const errorEmail = document.getElementById("error-Email")
 const errorPhone = document.getElementById("error-Phone")
 const errorTicket = document.getElementById("error-ticket")
+const errorBusTicket = document.getElementById("error-BusTicket")
+const errorAddress = document.getElementById("error-Address")
 
 const btnStep1 = document.getElementById("btnStep1")
 const btnStep2 = document.getElementById("btnStep2")
+const btnStep3 = document.getElementById("btnStep3")
 const btnConfirm = document.getElementById("btnConfirm")
 
 const btnBack2 = document.getElementById("btnBack2")
 const btnBack3 = document.getElementById("btnBack3")
+const btnBack4 = document.getElementById("btnBack4")
 const btnNew = document.getElementById("btnNew")
 const btnList = document.getElementById("btnList")
+
+const busRadios = document.querySelectorAll("input[name='busTicket']")
 
 const registerState = {
     step: 1,
@@ -28,12 +35,14 @@ const registerState = {
         phone: ""
     },
     ticket: "",
+    busTicket:"",
+    address:"",
     services: []
 }
 
 function renderStepLocation(){
     let html = ""
-    for(let i=1; i<= 3; i++){
+    for(let i=1; i<= 4; i++){
         if(i === registerState.step){
             html += `Step ${i} (current) | `
         }else if ( i < registerState.step){
@@ -52,10 +61,9 @@ function renderStepLocation(){
     i=3 → 3 !== 2 → true → Step 3 ẩn
  */
 function renderStep(){ 
-    for(let i=1; i<=3; i++){
+    for(let i=1; i<=4; i++){
         const stepSection = document.getElementById(`step-${i}`) 
         stepSection.hidden = i !== registerState.step
-        console.log("step",stepLocation)
     }
     renderStepLocation()
 }
@@ -119,9 +127,47 @@ function validate(){
         registerState.services = services
         
     }
+    if(registerState.step === 3){
+        errorBusTicket.textContent = ""
+        const busTicketChecked = document.querySelector(
+            "input[name='busTicket']:checked"
+        ) // :checked → chỉ chọn input đang được chọn (checked)
+       
+        if(!busTicketChecked){
+            errorBusTicket.textContent = "Vui lòng chọn Có hoặc Không"
+            isValid = false
+        }else if (busTicketChecked.value === "Co"){
+            const addressValue = addressInput.value.trim()
+            errorAddress.textContent = ""
+            if(!addressValue){
+                errorAddress.textContent = "vui lòng nhập địa chỉ"
+                isValid = false
+            }else {
+                registerState.address = addressValue
+                registerState.busTicket = busTicketChecked.value
+            }
+        }else {
+            registerState.busTicket = busTicketChecked.value
+            registerState.address = ""
+        }
+    }
     return isValid
 }
 
+busRadios.forEach(radio => {
+    radio.addEventListener ("change", () => {
+        const busTicketChecked = document.querySelector(
+            "input[name='busTicket']:checked"
+        )
+        
+        addressInput.hidden = busTicketChecked?.value !== "Co"
+
+        if (busTicketChecked?.value !== "Co") {
+            addressInput.value = ""
+            errorAddress.textContent = ""
+        }
+    })
+})
 btnStep1?.addEventListener("click", () => {
     if(validate()){
         registerState.step = 2
@@ -133,17 +179,22 @@ btnStep2?.addEventListener("click", () =>{
     if(validate()){
         registerState.step = 3
         renderStep()
+    }
+})
+btnStep3?.addEventListener("click", () =>{
+    if(validate()){
+        registerState.step = 4
+        renderStep()
         renderConfirm()
     }
 })
-
 btnBack2?.addEventListener("click", () => {
   registerState.step = 1
   renderStep()
 })
 
 function renderConfirm() {
-  const { personalInfo, ticket, services } = registerState
+  const { personalInfo, ticket, services, busTicket, address  } = registerState
 
   confirmInfo.innerHTML = `
     <p>Họ tên: ${personalInfo.name}</p>
@@ -151,12 +202,18 @@ function renderConfirm() {
     <p>SĐT: ${personalInfo.phone}</p>
     <p>Loại vé: ${ticket}</p>
     <p>Dịch vụ: ${services.length ? services.join(", ") : "Không có"}</p>
+    <p>Lên xe: ${busTicket}</p>
+    <p>Địa chỉ: ${address ? address : ""}</p>
   `
 }
 
 btnBack3?.addEventListener("click", () => {
   registerState.step = 2
   renderStep()
+})
+btnBack4?.addEventListener("click", () =>{
+    registerState.step = 3
+    renderStep()
 })
 
 btnConfirm?.addEventListener("click", () => {
@@ -166,7 +223,9 @@ btnConfirm?.addEventListener("click", () => {
     email: registerState.personalInfo.email,
     phone: registerState.personalInfo.phone,
     ticket: registerState.ticket,
-    services: registerState.services
+    services: registerState.services,
+    busTicket: registerState.busTicket,
+    address: registerState.address
   })
   window.location.href = "success.html"
 })
